fea4ee562d23c45a63824ed96d54cea319e80b60
MadAndrei <andreimirskov@yandex.ru>
2019-11-30 15:11:57 +0300

    fix

--- loderunnerclient/Main.py
+++ loderunnerclient/Main.py
@@ -39,10 +39,29 @@ def turn(gcb: Board, strboard=""):
     print("STUFF:\n",brd.get_at(0,0))
     #print(brd.to_string())
     #print(lib.foo())
-    b=not b
-    return LoderunnerAction.DRILL_LEFT if b else LoderunnerAction.DRILL_RIGHT
+    #b=not b
+    #return LoderunnerAction.DRILL_LEFT if b else LoderunnerAction.DRILL_RIGHT
     #action_id = random.randint(0, len(LoderunnerAction)-1)
     #return list(LoderunnerAction)[action_id]
+
+    _pos = brd.get_my_position()
+    pos = [_pos.get_x(), _pos.get_y()]
+    try:
+        move.dfs(pos[0], pos[1], 0, brd)
+        move.dfs_g(move.stack[0][0], move.stack[0][1], pos[0], pos[1], brd)
+        to_coord = move.path_list[-move.stack[0][2]:][0]
+    except:
+        return random.choice((LoderunnerAction.GO_RIGHT, LoderunnerAction.GO_LEFT))
+    if to_coord[0] > pos[0]:
+        return LoderunnerAction.GO_RIGHT
+    elif to_coord[0] < pos[0]:
+        return LoderunnerAction.GO_LEFT
+    elif to_coord[1] > pos[1]:
+        return LoderunnerAction.GO_UP
+    else:
+        return LoderunnerAction.GO_DOWN
+
+
 
 if __name__ == '__main__':
 
@@ -53,20 +72,31 @@ if __name__ == '__main__':
     userId="ocbpqfpr3dnpk82l0u5y"
     userCode="1969960948607981448"
 
-    b="☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼                             ~~~~~~~~~          (~~~~~~~☼☼##H########################H#H     @ H##########H    $  ☼☼( H                        H######H  H      (   H#☼☼☼☼☼#☼☼H☼☼#☼☼H    H#########H (   H#&    H#####H#####H##((~~~~~☼☼H     H    H         H#####H#)    H ~   H  @  H @ЄЄ((  (☼☼H#☼#☼#H    H         H  ~~~ #####H#     H     H    ЄЄ(((☼☼H  ~  HЄ~Є~H~~~~~~   H         & H   H######H##     @ЄЄ(☼☼H  @  H    H     H###☼☼☼☼☼☼H☼ $  H~~~H      H         $#☼☼H   ( H    H#####H         H     H      H#########H $ @$☼☼☼###☼##☼##☼H         H###H##    H##     H#       ##     ☼☼☼###☼~~~~  H   & &   H   H######H######### H###H #####H#☼☼☼$((☼      H   ~~~~~~H   H      H          H# #H      H ☼☼########H###☼☼☼☼     H  ############   ###### ##########☼☼        H            H                      ((         @☼☼H##########################H########~~~####H############☼☼H                 ~~~      H    @          H $          ☼☼#######H#######            H###~~~~      ############H  ☼☼&      H~~~~~~~~~~         H                         H  ☼☼       H    ##H   #######H##########~~~~~~>H######## H  ☼☼ ((    H    ##H (        H     $   &       H         H  ☼☼##H#####    ########H#######~~~~  ~~~#########>~~~~  H  ☼☼  H      (          H    @                       ~~~ЄH $☼☼#########H##########H  $ @   #☼☼☼☼☼☼#   ☼☼☼☼☼☼☼      H  ☼☼((&      H          H        ~ (((((~     (          H  ☼☼☼☼&      H~~Є~~~~~~~H       « ######   ###########   H  ☼☼    H###### &       #######H           ~~~~~~~~~~~~~~H  ☼☼H☼  H         (            H  H####H )               H  ☼☼H☼☼#☼☼☼☼☼☼☼☼☼☼☼☼###☼☼☼☼☼☼☼☼H☼☼☼☼☼☼☼☼ ☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼#☼☼H $ @&       ~~H~~~~☼☼☼☼☼☼☼H☼☼☼☼☼☼☼       H @ ~~~~~~~~~H☼☼H~~~~  ###### &H       & H☼H☼H    (   ####H  ☼    $    H☼☼H       (   @  ##H#######H☼H☼H######H  &  ###☼☼☼☼☼☼☼☼ ~H☼☼H#########       H&   ~~~H☼H☼H~~~   H~~~~~ ##&(((((@ ~ H☼☼H        ###H####H##H (   ☼H☼       H     ###☼☼☼☼☼☼ ~  H☼☼H       &   H   @  #######☼H☼#####  H##### $ ~~~~~~~ ~ H☼☼~ЄЄ~~~~~~~~~H       H~~~~~☼H☼~~~~~  H             ~ ~  H☼☼  &  H              H ((($☼H☼     ##########H (        H☼☼ ### #############H H#####☼H☼               H ######## H☼☼H   (@            H     $$☼H☼#######   (    H @  (     H☼☼H#####         H##H####                ###H#########   H☼☼H      H######### H   ############        H            H☼☼H##    H          H~~~~~~                 H #######H## H☼☼~~~~#####H#   ~~~~H         ########H   & H    $ & H   H☼☼         H ►      H      ~~~~~~~~   H     H        H   H☼☼   ####### H   «######H##        ##############    H  (H☼☼           H(    $$   H        ~~~~~           ##H#####H☼☼H (  ###########H     H#####H      ( @H##H       H &   H☼☼H###            H(    H     ###########  ##H###  H     H☼☼H  ######  ##H######  H          (  $      H   ##H###  H☼☼H            H ~~~~~##H###H  @  #########H##           H☼☼    H########H#       H   ######         H    (        H☼☼ ###H        H         ~~~~~H      ##H###H####H###     H☼☼    H########H#########     H  @  (  H        H        H☼☼H   H                       H     (  H        H        H☼☼H  ####H######         #####H########H##      H#####   H☼☼H      H      H#######Q   (       (           H        H☼☼##############H    )  H#################################☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼"
-    br=Board(b)
-    br.print_board()
+    #b="☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼                             ~~~~~~~~~          (~~~~~~~☼☼##H########################H#H     @ H##########H    $  ☼☼( H                        H######H  H      (   H#☼☼☼☼☼#☼☼H☼☼#☼☼H    H#########H (   H#&    H#####H#####H##((~~~~~☼☼H     H    H         H#####H#)    H ~   H  @  H @ЄЄ((  (☼☼H#☼#☼#H    H         H  ~~~ #####H#     H     H    ЄЄ(((☼☼H  ~  HЄ~Є~H~~~~~~   H         & H   H######H##     @ЄЄ(☼☼H  @  H    H     H###☼☼☼☼☼☼H☼ $  H~~~H      H         $#☼☼H   ( H    H#####H         H     H      H#########H $ @$☼☼☼###☼##☼##☼H         H###H##    H##     H#       ##     ☼☼☼###☼~~~~  H   & &   H   H######H######### H###H #####H#☼☼☼$((☼      H   ~~~~~~H   H      H          H# #H      H ☼☼########H###☼☼☼☼     H  ############   ###### ##########☼☼        H            H                      ((         @☼☼H##########################H########~~~####H############☼☼H                 ~~~      H    @          H $          ☼☼#######H#######            H###~~~~      ############H  ☼☼&      H~~~~~~~~~~         H                         H  ☼☼       H    ##H   #######H##########~~~~~~>H######## H  ☼☼ ((    H    ##H (        H     $   &       H         H  ☼☼##H#####    ########H#######~~~~  ~~~#########>~~~~  H  ☼☼  H      (          H    @                       ~~~ЄH $☼☼#########H##########H  $ @   #☼☼☼☼☼☼#   ☼☼☼☼☼☼☼      H  ☼☼((&      H          H        ~ (((((~     (          H  ☼☼☼☼&      H~~Є~~~~~~~H       « ######   ###########   H  ☼☼    H###### &       #######H           ~~~~~~~~~~~~~~H  ☼☼H☼  H         (            H  H####H )               H  ☼☼H☼☼#☼☼☼☼☼☼☼☼☼☼☼☼###☼☼☼☼☼☼☼☼H☼☼☼☼☼☼☼☼ ☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼#☼☼H $ @&       ~~H~~~~☼☼☼☼☼☼☼H☼☼☼☼☼☼☼       H @ ~~~~~~~~~H☼☼H~~~~  ###### &H       & H☼H☼H    (   ####H  ☼    $    H☼☼H       (   @  ##H#######H☼H☼H######H  &  ###☼☼☼☼☼☼☼☼ ~H☼☼H#########       H&   ~~~H☼H☼H~~~   H~~~~~ ##&(((((@ ~ H☼☼H        ###H####H##H (   ☼H☼       H     ###☼☼☼☼☼☼ ~  H☼☼H       &   H   @  #######☼H☼#####  H##### $ ~~~~~~~ ~ H☼☼~ЄЄ~~~~~~~~~H       H~~~~~☼H☼~~~~~  H             ~ ~  H☼☼  &  H              H ((($☼H☼     ##########H (        H☼☼ ### #############H H#####☼H☼               H ######## H☼☼H   (@            H     $$☼H☼#######   (    H @  (     H☼☼H#####         H##H####                ###H#########   H☼☼H      H######### H   ############        H            H☼☼H##    H          H~~~~~~                 H #######H## H☼☼~~~~#####H#   ~~~~H         ########H   & H    $ & H   H☼☼         H ►      H      ~~~~~~~~   H     H        H   H☼☼   ####### H   «######H##        ##############    H  (H☼☼           H(    $$   H        ~~~~~           ##H#####H☼☼H (  ###########H     H#####H      ( @H##H       H &   H☼☼H###            H(    H     ###########  ##H###  H     H☼☼H  ######  ##H######  H          (  $      H   ##H###  H☼☼H            H ~~~~~##H###H  @  #########H##           H☼☼    H########H#       H   ######         H    (        H☼☼ ###H        H         ~~~~~H      ##H###H####H###     H☼☼    H########H#########     H  @  (  H        H        H☼☼H   H                       H     (  H        H        H☼☼H  ####H######         #####H########H##      H#####   H☼☼H      H      H#######Q   (       (           H        H☼☼##############H    )  H#################################☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼"
+    #br=Board(b)
+    #br.print_board()
     #str = lib.foo(b)
     #print(":")
     #for c in _ELEMENTS:
     #    print(c,_ELEMENTS[c])
-    print("\n")
+    '''print("\n")
     _pos = br.get_my_position()
     pos = [_pos.get_x(),_pos.get_y()]
     print("pos:\t",pos)
     print("\n")
     print(move.dfs(pos[0],pos[1],0,br))
     print(move.stack)
-    #gcb = GameClient(url, userId, userCode)
-    #gcb.run(turn)
+    move.dfs_g(move.stack[0][0], move.stack[0][1], pos[0],pos[1], br)
+    to_coord = move.path_list[-move.stack[0][2]:][0]
+    if to_coord[0] > pos[0]:
+        return LoderunnerAction.GO_RIGHT
+    elif to_coord[0] < pos[0]:
+        return LoderunnerAction.GO_LEFT
+    elif to_coord[1] > pos[1]:
+        return LoderunnerAction.GO_UP
+    else:
+        return LoderunnerAction.GO_DOWN'''
+    #print(move.path_list)
+    gcb = GameClient(url, userId, userCode)
+    gcb.run(turn)
 
--- loderunnerclient/move.py
+++ loderunnerclient/move.py
@@ -75,3 +75,73 @@ def dfs(x,y, dist, brd):
     if not found:
         return False
     return True
+
+size=58
+
+path_list = []
+
+new_used = []
+
+flag = False
+
+def dfs_g(x_goal, y_goal, x , y, brd):
+    global flag
+    if flag:
+        return
+    path_list.append([x, y])
+
+    global size
+    global new_used
+
+    if (new_used == []):
+        new_used = [[0 for i in range(size)] for j in range(size)]
+
+    new_used[x][y] = 1
+
+    if (x == x_goal and y == y_goal):
+        flag = True
+        return
+
+        # go
+
+        # go LEFT
+    _x = x - 1
+    _y = y
+    brd.get_at(_x, _y).get_char() == '#'
+    if (not new_used[_x][_y] and 0 <= _x < size and 0 <= _y < size):  # in bounds and not visited
+        if (brd.has_ladder_at(_x, _y) or brd.has_pipe_at(_x, _y) or (
+                _y < size - 1 and (brd.get_at(_x, _y + 1).get_char() == '#'))):  # Walkable
+            dfs_g(x_goal, y_goal, _x, _y, brd)
+
+    # go RIGHT
+    _x = x + 1
+    _y = y
+
+    if (not new_used[_x][_y] and 0 <= _x < size and 0 <= _y < size):  # in bounds and not visited
+        if (brd.has_ladder_at(_x, _y) or brd.has_pipe_at(_x, _y) or (
+                _y < size - 1 and (brd.get_at(_x, _y + 1).get_char() == '#'))):  # Walkable
+            dfs_g(x_goal, y_goal, _x, _y, brd)
+
+    # go UP
+    _x = x
+    _y = y - 1
+
+    if (not new_used[_x][_y] and 0 <= _x < size and 0 <= _y < size):  # in bounds and not visited
+        if (brd.has_ladder_at(_x, _y) or brd.has_pipe_at(_x, _y) or (
+                _y < size - 1 and (brd.get_at(_x, _y + 1).get_char() == '#'))):  # Walkable
+            dfs_g(x_goal, y_goal, _x, _y, brd)
+
+    # go DOWN
+    _x = x
+    _y = y + 1
+
+    if (not new_used[_x][_y] and 0 <= _x < size and 0 <= _y < size):  # in bounds and not visited
+        if (brd.has_ladder_at(_x, _y) or brd.has_pipe_at(_x, _y) or (
+                _y < size - 1 and (brd.get_at(_x, _y + 1).get_char() == '#'))):  # Walkable
+            dfs_g(x_goal, y_goal, _x, _y, brd)
+    if not flag:
+        path_list.pop()
+
+
+
+
