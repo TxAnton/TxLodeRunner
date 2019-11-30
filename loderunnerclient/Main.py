9fdedd60b583183de22fdaaf80ea66ce2490c071
Anton <larin.an10n@gmail.com>
2019-11-30 13:46:42 +0300

    dfs main

--- loderunnerclient/Main.py
+++ loderunnerclient/Main.py
@@ -9,6 +9,8 @@ import random
 import logging
 import os
 
+import move
+
 #from loderunnerclient.internals.actions import LoderunnerAction
 #from loderunnerclient.internals.board import Board
 
@@ -26,40 +28,45 @@ i=0
 def turn(gcb: Board, strboard=""):
     global b
     global i
-    i+=1
-    if(i>4):
-        qq=lib.foo(strboard)
-        i=0
+    global brd
     #qq=lib.foo(strboard)
     print(strboard)
     brd = Board(strboard)
-    print(brd.get_barriers())
-    print(brd.get_my_position())
-    print(brd.get_gold_positions())
+    #brd.print_board()
+    #print("BARRIERS:\n",brd.get_barriers())
+    #print("ME:\n",brd.get_my_position())
+    #print("GOLD:",brd.get_gold_positions())
+    print("STUFF:\n",brd.get_at(0,0))
     #print(brd.to_string())
     #print(lib.foo())
     b=not b
     return LoderunnerAction.DRILL_LEFT if b else LoderunnerAction.DRILL_RIGHT
-    action_id = random.randint(0, len(LoderunnerAction)-1)
-    return list(LoderunnerAction)[action_id]
+    #action_id = random.randint(0, len(LoderunnerAction)-1)
+    #return list(LoderunnerAction)[action_id]
 
 if __name__ == '__main__':
-    url = "codingdojo2019.westeurope.cloudapp.azure.com"
-    userId="jlebg3nx316vg6qfo6q1"
-    userCode="7303406248495085684"
 
+    #url = "codingdojo2019.westeurope.cloudapp.azure.com"
+    #userId="jlebg3nx316vg6qfo6q1"
+    #userCode="7303406248495085684"
+    url = "codebattle-spb-2019.francecentral.cloudapp.azure.com"
+    userId="ocbpqfpr3dnpk82l0u5y"
+    userCode="1969960948607981448"
 
     b="☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼                             ~~~~~~~~~          (~~~~~~~☼☼##H########################H#H     @ H##########H    $  ☼☼( H                        H######H  H      (   H#☼☼☼☼☼#☼☼H☼☼#☼☼H    H#########H (   H#&    H#####H#####H##((~~~~~☼☼H     H    H         H#####H#)    H ~   H  @  H @ЄЄ((  (☼☼H#☼#☼#H    H         H  ~~~ #####H#     H     H    ЄЄ(((☼☼H  ~  HЄ~Є~H~~~~~~   H         & H   H######H##     @ЄЄ(☼☼H  @  H    H     H###☼☼☼☼☼☼H☼ $  H~~~H      H         $#☼☼H   ( H    H#####H         H     H      H#########H $ @$☼☼☼###☼##☼##☼H         H###H##    H##     H#       ##     ☼☼☼###☼~~~~  H   & &   H   H######H######### H###H #####H#☼☼☼$((☼      H   ~~~~~~H   H      H          H# #H      H ☼☼########H###☼☼☼☼     H  ############   ###### ##########☼☼        H            H                      ((         @☼☼H##########################H########~~~####H############☼☼H                 ~~~      H    @          H $          ☼☼#######H#######            H###~~~~      ############H  ☼☼&      H~~~~~~~~~~         H                         H  ☼☼       H    ##H   #######H##########~~~~~~>H######## H  ☼☼ ((    H    ##H (        H     $   &       H         H  ☼☼##H#####    ########H#######~~~~  ~~~#########>~~~~  H  ☼☼  H      (          H    @                       ~~~ЄH $☼☼#########H##########H  $ @   #☼☼☼☼☼☼#   ☼☼☼☼☼☼☼      H  ☼☼((&      H          H        ~ (((((~     (          H  ☼☼☼☼&      H~~Є~~~~~~~H       « ######   ###########   H  ☼☼    H###### &       #######H           ~~~~~~~~~~~~~~H  ☼☼H☼  H         (            H  H####H )               H  ☼☼H☼☼#☼☼☼☼☼☼☼☼☼☼☼☼###☼☼☼☼☼☼☼☼H☼☼☼☼☼☼☼☼ ☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼#☼☼H $ @&       ~~H~~~~☼☼☼☼☼☼☼H☼☼☼☼☼☼☼       H @ ~~~~~~~~~H☼☼H~~~~  ###### &H       & H☼H☼H    (   ####H  ☼    $    H☼☼H       (   @  ##H#######H☼H☼H######H  &  ###☼☼☼☼☼☼☼☼ ~H☼☼H#########       H&   ~~~H☼H☼H~~~   H~~~~~ ##&(((((@ ~ H☼☼H        ###H####H##H (   ☼H☼       H     ###☼☼☼☼☼☼ ~  H☼☼H       &   H   @  #######☼H☼#####  H##### $ ~~~~~~~ ~ H☼☼~ЄЄ~~~~~~~~~H       H~~~~~☼H☼~~~~~  H             ~ ~  H☼☼  &  H              H ((($☼H☼     ##########H (        H☼☼ ### #############H H#####☼H☼               H ######## H☼☼H   (@            H     $$☼H☼#######   (    H @  (     H☼☼H#####         H##H####                ###H#########   H☼☼H      H######### H   ############        H            H☼☼H##    H          H~~~~~~                 H #######H## H☼☼~~~~#####H#   ~~~~H         ########H   & H    $ & H   H☼☼         H ►      H      ~~~~~~~~   H     H        H   H☼☼   ####### H   «######H##        ##############    H  (H☼☼           H(    $$   H        ~~~~~           ##H#####H☼☼H (  ###########H     H#####H      ( @H##H       H &   H☼☼H###            H(    H     ###########  ##H###  H     H☼☼H  ######  ##H######  H          (  $      H   ##H###  H☼☼H            H ~~~~~##H###H  @  #########H##           H☼☼    H########H#       H   ######         H    (        H☼☼ ###H        H         ~~~~~H      ##H###H####H###     H☼☼    H########H#########     H  @  (  H        H        H☼☼H   H                       H     (  H        H        H☼☼H  ####H######         #####H########H##      H#####   H☼☼H      H      H#######Q   (       (           H        H☼☼##############H    )  H#################################☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼"
-    #br=Board(b)
-    #br.print_board()
+    br=Board(b)
+    br.print_board()
     #str = lib.foo(b)
     #print(":")
-    for c in _ELEMENTS:
-        print(c,_ELEMENTS[c])
+    #for c in _ELEMENTS:
+    #    print(c,_ELEMENTS[c])
+    print("\n")
+    _pos = br.get_my_position()
+    pos = [_pos.get_x(),_pos.get_y()]
+    print("pos:\t",pos)
+    print("\n")
+    print(move.dfs(pos[0],pos[1],0,br))
+    print(move.stack)
+    #gcb = GameClient(url, userId, userCode)
+    #gcb.run(turn)
 
-
-
-    gcb = GameClient(url, userId, userCode)
-    gcb.run(turn)
-
-    print(str)
